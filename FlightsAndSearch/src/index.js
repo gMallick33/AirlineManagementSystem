d501f0d191739d44b8787b8af461f5af793d38fb
gyani <g.arya.mallick@gmail.com>
2023-09-29 13:15:14 +0530

    fixed a minor bug

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -5,7 +5,7 @@ const { PORT } = require('./config/serverConfig');
 const setupAndStartServer = async () =>{
     const app = express();
     app.use(bodyParser.json);
-    app.use(bodyParser.urlencoded{express: true});
+    app.use(bodyParser.urlencoded({express: true}));
     app.listen(() => {
         console.log(`server started on: ${PORT}`);
         // console.log(process.env.PORT);
