242809b8e3f0467e9bf7eb76d2b151250955c5ff
gyani <g.arya.mallick@gmail.com>
2023-09-29 13:13:58 +0530

    implemented a basic body-parser code

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
 
 const setupAndStartServer = async () =>{
     const app = express();
+    app.use(bodyParser.json);
+    app.use(bodyParser.urlencoded{express: true});
     app.listen(() => {
         console.log(`server started on: ${PORT}`);
         // console.log(process.env.PORT);
