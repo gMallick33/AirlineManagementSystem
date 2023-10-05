e4d3532b27c70416e3edd8f7fb42f66f3b1ff48b
gyani <g.arya.mallick@gmail.com>
2023-10-05 15:26:22 +0530

    added city-service

---
+++ FlightsAndSearch/src/services/city-service.js
@@ -0,0 +1,47 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService {
+    constructor(){
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity(data){
+        try {
+            const city = await this.cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.log("something went wrong in service layer");
+            throw {error};
+        }
+    }
+
+    async deleteCity(cityId){
+        try {
+            const response = await this.cityRepository.deleteCity(cityId);
+            return response;
+        } catch (error) {
+            console.log("something went wrong in the service layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await this.cityRepository.updateCity(cityId, data);
+            return city;
+        } catch (error) {
+            console.log("something went wrong in the service layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await this.cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("something went wrong in the service layer");
+            throw {error};
+        }
+    }
+}
