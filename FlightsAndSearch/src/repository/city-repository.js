3fc342edfc51734be55623e1caf2c2c1a2723bce
gyani <g.arya.mallick@gmail.com>
2023-10-05 12:09:19 +0530

    completed city-repository

--- FlightsAndSearch/src/repository/city-repository.js
+++ FlightsAndSearch/src/repository/city-repository.js
@@ -3,21 +3,46 @@ const { City } = require('../models/index');
 class CityRepository{
     async createCity({ name }){
         try {
-            const city = City.create({ name });
+            const city = await City.create({ name });
             return city;
         } catch (error) {
+            console.log("something went wrong in repository layer");
             throw {error};
         }
     }
 
     async deleteCity(cityId){
         try {
-            City.destroy({
+            await City.destroy({
                 where: {
                     id: cityId
                 }
             });
         } catch (error) {
+            console.log("something went wrong in repository layer");
+            throw {error};
+        }
+    }
+
+    async updateCity(cityId, data){
+        try {
+            const city = await City.update(data, {
+                where: {
+                    id: cityId
+                }
+            })
+        } catch (error) {
+            console.log("something went wrong in the repository layer");
+            throw {error};
+        }
+    }
+
+    async getCity(cityId){
+        try {
+            const city = await City.findByPK(cityId);
+            return city;
+        } catch (error) {
+            console.log("something went wrong in repository layer");
             throw {error};
         }
     }
