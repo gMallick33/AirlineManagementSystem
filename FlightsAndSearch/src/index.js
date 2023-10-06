2967166ac80fc65ca0fcd771333bff6a820bdec6
gyani <g.arya.mallick@gmail.com>
2023-10-06 17:36:43 +0530

    minor fixes for json working

--- FlightsAndSearch/src/index.js
+++ FlightsAndSearch/src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
 
 const setupAndStartServer = async () =>{
     const app = express();
-    app.use(bodyParser.json);
-    app.use(bodyParser.urlencoded({express: true}));
-    app.listen(() => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use('/api', ApiRoutes);
+    app.listen(PORT, () => {
+        // console.log(PORT);
         console.log(`server started on: ${PORT}`);
-        // console.log(process.env.PORT);
+        console.log(PORT);
     })
 }
 
