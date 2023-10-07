b27b2c3d6d2ba1a9309710e4f097648dc68ab638
gyani <g.arya.mallick@gmail.com>
2023-10-07 19:49:34 +0530

    added and seeded airplanes

---
+++ FlightsAndSearch/src/seeders/20231007111736-add-airplanes.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   await queryInterface.bulkInsert('Airplanes',[
+    {
+      modelNumber: 'Boeing 737',
+      capacity: 350,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      modelNumber: 'Airbus A320',
+      capacity: 200,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      modelNumber: 'Boeing 777',
+      capacity: 300,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      modelNumber: 'Airbus A330',
+      capacity: 150,
+      createdAt: new Date(),
+      updatedAt: new Date()
+      
+    }
+   ], {})
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
