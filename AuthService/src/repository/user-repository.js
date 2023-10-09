d499bfe8370be3562cf3d709b4f85aa0714bbb6e
gyani <g.arya.mallick@gmail.com>
2023-10-09 23:19:59 +0530

    added getById in user repository layer

--- AuthService/src/repository/user-repository.js
+++ AuthService/src/repository/user-repository.js
@@ -23,6 +23,18 @@ class UserRepository {
             throw error;
         }
     }
+
+    async getById(userId) {
+        try {
+            const user = await User.findByPk(userId, {
+                attributes: ['email', 'id']
+            });
+            return user;
+        } catch (error) {
+            console.log("something went wrong on user repository layer");
+            throw error;
+        }
+    }
 }
 
 module.exports = UserRepository;
