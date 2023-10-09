a65f0dc35e838d23a02f7bd053a6ad90325f68fb
gyani <g.arya.mallick@gmail.com>
2023-10-09 23:52:44 +0530

    added checkPassword function

--- AuthService/src/services/user-service.js
+++ AuthService/src/services/user-service.js
@@ -1,5 +1,6 @@
 const UserRepository = require('../repository/user-repository');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 const { JWT_KEY } = require('../config/serverConfig');
 class UserService {
     constructor(){
@@ -32,6 +33,15 @@ class UserService {
             return response;
         } catch (error) {
             console.log('something went wrong in token validation');
+            throw error;
+        }
+    }
+
+    checkPassword(userInputPlainPassword, encryptedPassword){
+        try {
+            return bcrypt.compareSync(userInputPlainPassword, encryptedPassword);
+        } catch (error) {
+            console.log('something went wrong in password comparision');
             throw error;
         }
     }
