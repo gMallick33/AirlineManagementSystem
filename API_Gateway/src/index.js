fe4191935b2eb0d242f9e82081057d20671e3f8d
gyani <g.arya.mallick@gmail.com>
2023-10-14 16:24:20 +0530

    implemented middleware for bookingservice authentication

--- API_Gateway/src/index.js
+++ API_Gateway/src/index.js
@@ -3,7 +3,7 @@ const morgan = require('morgan');
 const { PORT } = require('./config/serverConfig');
 const app = express();
 const { createProxyMiddleware } = require('http-proxy-middleware');
-
+const axios = require('axios');
 const rateLimit = require('express-rate-limit');
 const limiter = rateLimit({
     windowMs: 2 * 60 * 1000,
@@ -12,6 +12,29 @@ const limiter = rateLimit({
 console.log(PORT);
 app.use(morgan('combined'));
 app.use(limiter);
+app.use('/bookingservice', async (req, res, next) => {
+    console.log(req.headers['x-access-token']);
+    try {
+        const response = await axios.get('http://localhost:3001/api/v1/isAuthenticated', {
+            headers: {
+                'x-access-token': req.headers['x-access-token']
+            }
+        });
+        console.log(response.data);
+        if(response.data.success) {
+            next();
+        }
+        else {
+            return res.status(401).json({
+                message: 'Unauthorized'
+            });
+        }
+    } catch (error) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+})
 app.use('/bookingservice', createProxyMiddleware({target: 'http://localhost:3002/', changeOrigin: true}));
 app.get('/home', (req, res) => {
     return res.json({
